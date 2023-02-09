c8d1bcca01ed525ae8f36a98faadcb63f22fb2b9
mr-dargi <dargimohammadreza@gmail.com>
2023-02-09 15:27:29 +0330

    Add some button styles and also input styles in JavaScript section with fetch method fetch json file data from a weather service server and use it inside the our webpage

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let weather = {
+    apiKey: "",      //your API you can create account in https://openweathermap.org and create your own API
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(data);
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".displayIcon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+    }
+}
